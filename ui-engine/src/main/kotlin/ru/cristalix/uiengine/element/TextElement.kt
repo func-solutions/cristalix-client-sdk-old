7fb3cb088b9dfd23d9aa404ddafda3c1fe36f498
delfikpro <33707322+DelfikPro@users.noreply.github.com>
2021-05-04 00:38:03 +0300

    feature: Multi-line text elements

--- ui-engine/src/main/kotlin/ru/cristalix/uiengine/element/TextElement.kt
+++ ui-engine/src/main/kotlin/ru/cristalix/uiengine/element/TextElement.kt
@@ -10,8 +10,29 @@ class TextElement() : AbstractElement() {
     var content: String = ""
         set(value) {
             field = value
-            this.size = V3(UIEngine.clientApi.fontRenderer().getStringWidth(value).toDouble(), 9.0)
+
+            lines = value.split("\n").toTypedArray()
+            lineWidthCache =
+                lines.map { UIEngine.clientApi.fontRenderer().getStringWidth(it).toDouble() }.toDoubleArray()
+
+            val emptyLines = lines.count { it.isBlank() }
+            this.size = V3(
+                lineWidthCache.maxOrNull() ?: 0.0,
+                lineHeight * (lines.size - emptyLines) + emptyLineHeight * emptyLines
+            )
         }
+
+    var lineHeight = 9.0
+        set(value) {
+            field = value
+            content = content
+        }
+
+    var emptyLineHeight = 9.0
+
+    internal var lines: Array<String> = arrayOf()
+
+    internal var lineWidthCache: DoubleArray = doubleArrayOf()
 
     var shadow = false
 
@@ -30,12 +51,23 @@ class TextElement() : AbstractElement() {
 
         // There is a weird behaviour of vanilla fontRenderer that disables transparency if the alpha value is lower than 5.
         // Perhaps that is yet another bodge by mojang to quickly implement some cool font-related effects.
-        // Anyways, that is quite unnoticable, so ui engine just discards these barely visible text elements.
+        // Anyways, that is quite unnoticeable, so ui engine just discards these barely visible text elements.
         if (alpha < 5) return
 
         if (alpha != 255) GlStateManager.enableBlend()
         val fontRenderer = UIEngine.clientApi.fontRenderer()
-        fontRenderer.drawString(this.content, 0f, if (fontRenderer.unicodeFlag) 0f else 1f, cachedHexColor, shadow)
+
+        lines.forEachIndexed { index, line ->
+
+            fontRenderer.drawString(
+                line,
+                ((size.x - lineWidthCache[index]) * origin.x).toFloat(),
+                if (fontRenderer.unicodeFlag) 0f else 1f, cachedHexColor, shadow
+            )
+            GlStateManager.translate(0.0, if (line.isBlank()) emptyLineHeight else lineHeight, 0.0);
+
+        }
+
     }
 
 }
