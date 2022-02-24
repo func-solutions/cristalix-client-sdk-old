e102f6a988d69531e47e51db9920277f5764453f
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-02-24 05:57:08 +0300

    Исправлен SphereElement, добавлен удобный метод создания, написаны комментарии

--- ui-engine/src/main/kotlin/ru/cristalix/uiengine/element/SphereElement.kt
+++ ui-engine/src/main/kotlin/ru/cristalix/uiengine/element/SphereElement.kt
@@ -12,12 +12,14 @@ class SphereElement : AbstractElement() {
 
     override fun render() {
         val mc = clientApi.minecraft()
+        // Переменные для того, чтобы при перемещении игрока, фигура стояла в нужном месте
         val entity = mc.renderViewEntity
         val pt = mc.timer.renderPartialTicks
         val prevX = entity.prevX
         val prevY = entity.prevY
         val prevZ = entity.prevZ
 
+        // GL начало
         GlStateManager.disableLighting()
         GlStateManager.disableTexture2D()
         GlStateManager.disableAlpha()
@@ -27,39 +29,51 @@ class SphereElement : AbstractElement() {
         GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE)
         GlStateManager.color(color.red.toFloat(), color.green.toFloat(), color.blue.toFloat(), color.alpha.toFloat())
 
+        // Количество вершин на срезе (можно разбить на два измерения)
         val accuracy = 20.0
-        val dx = offset.x - (entity.x - prevX) * pt - prevX
-        val dy = offset.y - (entity.y - prevY) * pt - prevY
-        val dz = offset.z - (entity.z - prevZ) * pt - prevZ
 
+        // Координаты сферы (если они есть и у родителя - координаты самой сферы становятся относительными)
+        val dx = (lastParent?.offset?.x ?: 0.0) + offset.x - (entity.x - prevX) * pt - prevX
+        val dy = (lastParent?.offset?.y ?: 0.0) + offset.y - (entity.y - prevY) * pt - prevY
+        val dz = (lastParent?.offset?.z ?: 0.0) + offset.z - (entity.z - prevZ) * pt - prevZ
+
+        // Начало отрисовки сферы
         var j: Int
         var i = 0
         while (i <= accuracy) {
-            val lat0: Double = Math.PI * (-0.5 + (i - 1).toDouble() / accuracy)
+            // Нахождение двух углов
+            val lat0 = Math.PI * (-0.5 + (i - 1).toDouble() / accuracy)
             val z0 = sin(lat0)
             val zr0 = cos(lat0)
-            val lat1: Double = Math.PI * (-0.5 + i.toDouble() / accuracy)
+            val lat1 = Math.PI * (-0.5 + i.toDouble() / accuracy)
             val z1 = sin(lat1)
             val zr1 = cos(lat1)
 
+            // Создание буффера
             val tessellator = clientApi.tessellator()
             val buffer = tessellator.bufferBuilder
 
-            j = 0
+            // Определение типа отрисовки
             buffer.begin(GL11.GL_QUAD_STRIP, DefaultVertexFormats.POSITION_NORMAL)
+            j = 0
             while (j <= accuracy) {
                 val lng = 2.0 * Math.PI * (j - 1.0) / accuracy
-                val x = cos(lng) * size.x
-                val y = sin(lng) * size.y
+                // Прочет x, y координат точек сферы
+                val x = cos(lng) * size.x * 10.0
+                val y = sin(lng) * size.y * 10.0
 
-                writeNormalAndPosition(buffer, dx + x * zr0, dy + y * zr0, dz + size.z * z0)
-                writeNormalAndPosition(buffer, dx + x * zr1, dy + y * zr1, dz + size.z * z1)
+                // Заполнение буффера вершинами и нормалями
+                writeNormalAndPosition(buffer, dx + x * zr0, dy + y * zr0, dz + size.z * 10.0 * z0)
+                writeNormalAndPosition(buffer, dx + x * zr1, dy + y * zr1, dz + size.z * 10.0 * z1)
                 j++
             }
+            // Отрисовка и чистка буффера
             tessellator.draw()
             i++
         }
 
+        // GL конец
+        GlStateManager.color(1f, 1f, 1f, 1f)
         GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_CONSTANT_COLOR)
         GlStateManager.shadeModel(GL11.GL_FLAT)
         GlStateManager.enableTexture2D()
